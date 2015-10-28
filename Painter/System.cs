5d6fbe6c7badd86f2f01b6e9fb3065881001856f
Daniel Buttery <hasanyoneseenmyspork+git@gmail.com>
2015-10-28 14:04:56 +0000

    Created methods for creating a Default.hue via the Tools object - this will need to actively bve maintained as features are added.

--- Painter/System.cs
+++ Painter/System.cs
@@ -71,7 +71,80 @@ namespace Painter
                 throw;
             }
         }
-        
+
+        /// <summary>
+        /// Creates a default hue file that can either be used for demo purposes or to replace a missing Default.hue (one should always be present).
+        /// </summary>
+        /// <param name="FileName">The name and path of the file to write to. If ommitted this will default to %APPDATA%\Default.hue .</param>
+        public void CreateDefaultHueFile(string FileName = "c:\test.hue")
+        {
+            Tools tools = new Tools();
+            tools.ExportConfig(CreateDefaultObject(), FileName);
+
+        }
+
+        private IOConfig.InputConfig CreateDefaultObject()
+        {
+            IOConfig.InputConfig cfg = new IOConfig.InputConfig();
+            cfg.Settings = DefaultSettings();
+            cfg.Inputs = DefaultRegionList();
+            cfg.Name = "Default Configuration";
+            return cfg;
+        }
+
+        /// <summary>
+        /// Creates a Settings object that mimics the configuration set by the user through the UI.
+        /// </summary>
+        /// <returns></returns>
+        private DetectionSettings DefaultSettings()
+        {
+            DetectionSettings settings = new DetectionSettings();
+            settings.ConserveMemory = false;
+            settings.ShowPreview = true;
+            settings.DisableFormDrawing = false;
+
+            settings.SampleWidth = 1;
+            settings.SampleAccuracy = 1;
+            settings.SampleInterval = 1;
+
+            return settings;
+
+        }
+
+        /// <summary>
+        /// A generated list of Regions for use during detection.
+        /// </summary>
+        /// <returns></returns>
+        private List<Input> DefaultRegionList()
+        {
+            List<Input> _regions = new List<Input>();
+            _regions.Add(SampleInput(0, 25, 75, 50, 50));
+            _regions.Add(SampleInput(1, 0, 50, 0, 0));
+            _regions.Add(SampleInput(2, 51, 100, 0, 0));
+            _regions.Add(SampleInput(3, 0, 50, 100, 100));
+            _regions.Add(SampleInput(4, 51, 100, 100, 100));
+            _regions.Add(SampleInput(5, 0, 0, 0, 50));
+            _regions.Add(SampleInput(6, 0, 0, 51, 100));
+            _regions.Add(SampleInput(7, 100, 100, 0, 50));
+            _regions.Add(SampleInput(8, 100, 100, 51, 100));
+            return _regions;
+        }
+
+        /// <summary>
+        /// Creates a sample Input region from hard coded values. Should only be used for testing. 
+        /// </summary>
+        /// <returns></returns>
+        private Input SampleInput(int ID, int startX, int endX, int startY, int endY)
+        {
+            Input region = new Input();
+            region.StartX = startX;
+            region.EndX = endX;
+            region.StartY = startY;
+            region.EndY = endY;
+            region.Description = "Sample input";
+            region.ID = ID;
+            return region;
+        }
         
     }
 }
