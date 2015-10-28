215defa4f85b31a746a00bca94c619236c7d499f
Daniel Buttery <hasanyoneseenmyspork+git@gmail.com>
2015-10-28 10:20:35 +0000

    Removed Region/Regions definitions as it was a duplication of Input/Inputs

--- Painter/Detector.cs
+++ Painter/Detector.cs
@@ -15,121 +15,7 @@ namespace Painter
     {
         private Tools.Log _log = new Tools.Log();
         private Image _image;
-
-        /// <summary>
-        /// A Region defines a line with which to begin obtaining sample data for a given image.
-        /// </summary>
-        public class Region
-        {
-            private int _startx;
-            private int _endx;
-            private int _starty;
-            private int _endy;
-            private int _id;
-            private string _name;
-            
-            /// <summary>
-            /// An Integer value representation of a whole number percentage (10 = 10%, 100 = 100%) detailing at what point the analysis line begins.
-            /// </summary>
-            public int StartX
-            {
-                get {
-                    return _startx;
-                }
-
-                set
-                {
-                    _startx = value;
-                }
-            }
-
-            /// <summary>
-            /// An Integer value representation of a whole number percentage (10 = 10%, 100 = 100%) detailing at what point the analysis line ends.
-            /// </summary>
-            public int EndX
-            {
-                get
-                {
-                    return _endx;
-                }
-
-                set
-                {
-                    _endx = value;
-                }
-            }
-
-            /// <summary>
-            /// An Integer value representation of a whole number percentage (10 = 10%, 100 = 100%) detailing at what point the analysis line begins.
-            /// </summary>
-            public int StartY
-            {
-                get
-                {
-                    return _starty;
-                }
-
-                set
-                {
-                    _starty = value;
-                }
-            }
-
-            /// <summary>
-            /// An Integer value representation of a whole number percentage (10 = 10%, 100 = 100%) detailing at what point the analysis line ends.
-            /// </summary>
-            public int EndY
-            {
-                get
-                {
-                    return _endy;
-                }
-
-                set
-                {
-                    _endy = value;
-                }
-            }
-
-            /// <summary>
-            /// An integer that represents the unioque ID of the Region.
-            /// </summary>
-            public int ID
-            {
-                get
-                {
-                    return _id;
-                }
-
-                set
-                {
-                    _id = value;
-                }
-            }
-
-            /// <summary>
-            /// A 'friendly', 'human' value describing the Region.
-            /// </summary>
-            public string Name
-            {
-                get
-                {
-                    return _name;
-                }
-
-                set
-                {
-                    _name = value;
-                }
-            }
-
-        }
         
-        /// <summary>
-        /// A collection of Region objects that define the areas that need to be analyzed.
-        /// </summary>
-        public List<Region> Regions { get; set; }
-
         /// <summary>
         /// Returns a color object that represents the average colour of an image based on the settings provided.
         /// </summary>
@@ -157,6 +43,7 @@ namespace Painter
                 int alpha = 0;
                 int xcounter = 0;
                 var ycounter = 0;
+
                 for (int x = (startx * xpc); x < (endx * xpc); x = x + accuracy)
                 {
                     ycounter=0;
